54c80d4f6015479f0c2776f5c984aef95d35905d
Ruslan <correcttechno@gmail.com>
2023-01-19 10:03:38 +0400

    ch

--- client.py
+++ client.py
@@ -1,23 +1,44 @@
 import cv2
-import numpy as np
+import io
 import socket
-import sys
-import pickle
 import struct
+import time
+import pickle
+import numpy as np
+import imutils
 
-cap=cv2.VideoCapture(0)
-clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('192.168.16.106',8089))
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client_socket.connect(('0.tcp.ngrok.io', 19194))
+client_socket.connect(('192.168.16.106', 8090))
+
+cam = cv2.VideoCapture(0)
+img_counter = 0
+
+#encode to jpeg format
+#encode param image quality 0 to 100. default:95
+#if you want to shrink data size, choose low image quality.
+encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
 
 while True:
-    ret,frame=cap.read()
-    if(cap.isOpened()):
-        # Serialize frame
-        data = pickle.dumps(frame)
+    ret, frame = cam.read()
+    # 影像縮放
+    frame = imutils.resize(frame, width=320)
+    # 鏡像
+    frame = cv2.flip(frame,180)
+    result, image = cv2.imencode('.jpg', frame, encode_param)
+    data = pickle.dumps(image, 0)
+    size = len(data)
 
-        # Send message length first
-        message_size = struct.pack("L", len(data)) ### CHANGED
+    if img_counter%10==0:
+        client_socket.sendall(struct.pack(">L", size) + data)
+        #cv2.imshow('client',frame)
+        
+    img_counter += 1
 
-        # Then data
-        clientsocket.sendall(message_size + data)
-        cv2.imshow('Client', frame)
+    # 若按下 q 鍵則離開迴圈
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+
+cam.release()
--- server.py
+++ server.py
@@ -1,45 +1,48 @@
-import pickle
+# Import the required modules
 import socket
-import struct
-
+import sys
 import cv2
+import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+import struct ## new
+import zlib
+from PIL import Image, ImageOps
 
-HOST = '192.168.16.106'
-PORT = 8089
+HOST='192.168.16.106'
+PORT=8090
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 print('Socket created')
-socket_address = (HOST,PORT)
-s.bind((socket_address))
+
+s.bind((HOST,PORT))
 print('Socket bind complete')
-s.listen(5)
+s.listen(10)
 print('Socket now listening')
 
-conn, addr = s.accept()
+conn,addr=s.accept()
 
-data = b'' ### CHANGED
-payload_size = struct.calcsize("L") ### CHANGED
-
+data = b""
+payload_size = struct.calcsize(">L")
+print("payload_size: {}".format(payload_size))
 while True:
-
-    # Retrieve message size
     while len(data) < payload_size:
-        data += conn.recv(20*1024)
-
+        data += conn.recv(4096)
+        if not data:
+            cv2.destroyAllWindows()
+            conn,addr=s.accept()
+            continue
+    # receive image row data form client socket
     packed_msg_size = data[:payload_size]
     data = data[payload_size:]
-    msg_size = struct.unpack("L", packed_msg_size)[0] ### CHANGED
-
-    # Retrieve all data based on message size
+    msg_size = struct.unpack(">L", packed_msg_size)[0]
     while len(data) < msg_size:
-        data += conn.recv(20*1024)
-
+        data += conn.recv(4096)
     frame_data = data[:msg_size]
     data = data[msg_size:]
+    # unpack image using pickle 
+    frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
 
-    # Extract frame
-    frame = pickle.loads(frame_data)
-
-    # Display
-    cv2.imshow('SERVER', frame)
+    cv2.imshow('SERVER',frame)
     cv2.waitKey(1)
