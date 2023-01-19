e4ac02f87b001c4a8f049423ea53aebf77b646cd
Ruslan <correcttechno@gmail.com>
2023-01-19 11:10:24 +0400

    changes

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import pickle
 import numpy as np
 import struct ## new
+import threading
 import zlib
 from PIL import Image, ImageOps
 
@@ -17,32 +18,77 @@ print('Socket created')
 
 s.bind((HOST,PORT))
 print('Socket bind complete')
-s.listen(10)
+s.listen(5)
 print('Socket now listening')
+MYFR=[None,None]
 
-conn,addr=s.accept()
 
-data = b""
-payload_size = struct.calcsize(">L")
-print("payload_size: {}".format(payload_size))
+def startLive( index):
+    global MYFR
+    conn,addr=s.accept()
+
+    data = b""
+    payload_size = struct.calcsize(">L")
+    print("payload_size: {}".format(payload_size))
+    while True:
+        while len(data) < payload_size:
+            data += conn.recv(20*1024)
+            if not data:
+                cv2.destroyAllWindows()
+                conn,addr=s.accept()
+                continue
+        # receive image row data form client socket
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack(">L", packed_msg_size)[0]
+        while len(data) < msg_size:
+            data += conn.recv(20*1024)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+        # unpack image using pickle 
+        frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+        frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+        MYFR[index]=frame
+        
+
+def startCam( ):
+    global MYFR
+    conn,addr=s.accept()
+
+    data = b""
+    payload_size = struct.calcsize(">L")
+    print("payload_size: {}".format(payload_size))
+    while True:
+        while len(data) < payload_size:
+            data += conn.recv(20*1024)
+            if not data:
+                cv2.destroyAllWindows()
+                conn,addr=s.accept()
+                continue
+        # receive image row data form client socket
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack(">L", packed_msg_size)[0]
+        while len(data) < msg_size:
+            data += conn.recv(20*1024)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+        # unpack image using pickle 
+        frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+        frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+        #MYFR[index]=frame
+       
+
+
+
+
+p1 = threading.Thread(target=startLive,args={0})
+p1.start()
+
+p2 = threading.Thread(target=startCam)
+p2.start()
+
 while True:
-    while len(data) < payload_size:
-        data += conn.recv(4096)
-        if not data:
-            cv2.destroyAllWindows()
-            conn,addr=s.accept()
-            continue
-    # receive image row data form client socket
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack(">L", packed_msg_size)[0]
-    while len(data) < msg_size:
-        data += conn.recv(4096)
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-    # unpack image using pickle 
-    frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
-    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-
-    cv2.imshow('SERVER',frame)
+    if(MYFR[0] is not None):
+        cv2.imshow('FRAME 1',MYFR[0])
     cv2.waitKey(1)
