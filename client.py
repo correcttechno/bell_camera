61159b6aa02d906ce4963826da3065f0792eacac
Ruslan <correcttechno@gmail.com>
2023-01-20 16:09:20 +0400

    ch

--- client.py
+++ client.py
@@ -23,22 +23,25 @@ img_counter = 0
 encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
 
 while True:
-    ret, frame = cam.read()
-    frame = imutils.resize(frame, width=320)
- 
-    frame = cv2.flip(frame,180)
-    result, image = cv2.imencode('.jpg', frame, encode_param)
-    data = pickle.dumps(image, 0)
-    size = len(data)
+    if client_socket:
+        ret, frame = cam.read()
+        frame = imutils.resize(frame, width=320)
+    
+        frame = cv2.flip(frame,180)
+        result, image = cv2.imencode('.jpg', frame, encode_param)
+        data = pickle.dumps(image, 0)
+        size = len(data)
 
-    if img_counter%10==0:
-        client_socket.sendall(struct.pack(">L", size) + data)
-        #cv2.imshow('client',frame)
-        
-    img_counter += 1
+        if img_counter%10==0:
+            client_socket.sendall(struct.pack(">L", size) + data)
+            #cv2.imshow('client',frame)
+            
+        img_counter += 1
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        client_socket.connect((HOST, 8090))
     
 
 cam.release()
