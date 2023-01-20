e8f599d4f85010737af31b5eddf89d154171fd33
Ruslan <correcttechno@gmail.com>
2023-01-20 13:16:48 +0400

    ch

--- server.py
+++ server.py
@@ -38,27 +38,28 @@ def startLive( index):
     payload_size = struct.calcsize(">L")
     print("payload_size: {}".format(payload_size))
     while True:
-        while len(data) < payload_size:
-            data += conn.recv(50*1024)
-            MData=data
-            if not data:
-                cv2.destroyAllWindows()
-                conn,addr=s.accept()
-                continue
-        # receive image row data form client socket
-        packed_msg_size = data[:payload_size]
+        if conn:
+            while len(data) < payload_size:
+                data += conn.recv(50*1024)
+                MData=data
+                if not data:
+                    cv2.destroyAllWindows()
+                    conn,addr=s.accept()
+                    continue
+            # receive image row data form client socket
+            packed_msg_size = data[:payload_size]
 
-        data = data[payload_size:]
-       
-        msg_size = struct.unpack(">L", packed_msg_size)[0]
-        while len(data) < msg_size:
-            data += conn.recv(50*1024)
-        frame_data = data[:msg_size]
-        data = data[msg_size:]
-        # unpack image using pickle 
-        frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
-        frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-        MYFR[index]=frame
+            data = data[payload_size:]
+        
+            msg_size = struct.unpack(">L", packed_msg_size)[0]
+            while len(data) < msg_size:
+                data += conn.recv(50*1024)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            # unpack image using pickle 
+            frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+            frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+            MYFR[index]=frame
         
 
 def startCam( ):
