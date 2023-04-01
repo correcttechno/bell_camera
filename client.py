7aec6bf69b1d69add71c5d52f8ceec84e4abc526
Ruslan <correcttechno@gmail.com>
2023-04-01 11:06:32 +0400

    sd

--- client.py
+++ client.py
@@ -1,49 +1,54 @@
-import io
+import cv2
+import pyaudio
 import socket
 import struct
-import time
-import picamera
-import pyaudio
 
 # bağlantı parametreleri
-SERVER_IP = '192.168.16.106'
-SERVER_PORT =8077
-
-# Raspberry Pi kamera ayarları
-RESOLUTION = (640, 480)
-FRAMERATE = 24
-
-# ses kayıt ayarları
-CHUNK_SIZE = 1024
-SAMPLE_RATE = 44100
-RECORD_SECONDS = 1
+IP_ADDRESS = '192.168.16.106'
+PORT = 8077
 
 # TCP/IP soketi oluştur
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((SERVER_IP, SERVER_PORT))
+client_socket.connect((IP_ADDRESS, PORT))
 
-# Kamera örneği oluştur
-camera = picamera.PiCamera(resolution=RESOLUTION, framerate=FRAMERATE)
+# Kamera akışını başlat
+video_capture = cv2.VideoCapture(0)
 
-# Ses kayıt cihazı örneği oluştur
+# Görüntü kodlama ayarları
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+# PyAudio ayarları
+CHUNK_SIZE = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+# PyAudio örneği oluştur
 audio = pyaudio.PyAudio()
-stream = audio.open(format=pyaudio.paInt16, channels=1, rate=SAMPLE_RATE, input=True, frames_per_buffer=CHUNK_SIZE)
 
-# Görüntü akışını ve ses kaydını başlat
-video_stream = io.BytesIO()
+# Mikrofon akışını başlat
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    frames_per_buffer=CHUNK_SIZE)
+
 while True:
-    # Görüntüyü al
-    camera.capture(video_stream, format='jpeg', use_video_port=True)
-    # Görüntü boyutunu al
-    video_size = video_stream.tell()
-    # Ses verisini al
+    # Görüntü yakala
+    ret, frame = video_capture.read()
+    # Görüntüyü kodla
+    result, frame_encoded = cv2.imencode('.jpg', frame, encode_param)
+    # Ses verisi oku
     audio_data = stream.read(CHUNK_SIZE)
-    # Ses verisi boyutunu al
-    audio_size = len(audio_data)
-    # Görüntüyü ve ses verisini birleştir
-    data = struct.pack("!L", video_size) + video_stream.getvalue() + struct.pack("!L", audio_size) + audio_data
-    # Veri paketini gönder
-    client_socket.sendall(data)
-    # Veri akışlarını sıfırla
-    video_stream.seek(0)
-    video_stream.truncate()
+    # Veri boyutlarını hesapla
+    video_size = struct.pack('!L', len(frame_encoded))
+    audio_size = struct.pack('!L', len(audio_data))
+    # Verileri bir araya getir ve paketle
+    packet = video_size + frame_encoded.tobytes() + audio_size + audio_data
+    # Paketi gönder
+    client_socket.sendall(packet)
+
+# Kaynakları serbest bırak
+video_capture.release()
+stream.stop_stream()
+stream.close()
+audio.terminate()
+client_socket.close()
