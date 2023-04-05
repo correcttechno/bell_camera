2d2a97b8be96f7ab8cc51c3b2619d94fa0897af6
Ruslan <correcttechno@gmail.com>
2023-04-05 13:15:53 +0400

    asd

--- client.py
+++ client.py
@@ -1,87 +1,47 @@
 import cv2
-import io
-import socket
-import struct
-import time
-import pickle
-import numpy as np
-import imutils
 import pyaudio
+import numpy as np
 import threading
+import websocket
+import json
 
+# Video capture object
+cap = cv2.VideoCapture(0)
 
-# client_socket.connect(('0.tcp.ngrok.io', 19194))
-#HOST = '192.168.16.103'
-HOST = '162.214.48.246'
-CAMERAPORT = 8097
-SOUNDPORT=8094
+# Audio recording object
+audio = pyaudio.PyAudio()
+stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
 
-CHUNK_SIZE = 128
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
+# WebSocket client
+ws = websocket.WebSocket()
 
-
-cameraSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cameraSocket.connect((HOST, CAMERAPORT))
-
-soundSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-soundSocket.connect((HOST, SOUNDPORT))
-
-cam = cv2.VideoCapture(0)
-cam.set(cv2.CAP_PROP_FPS, 24)
-
-
-#encode to jpeg format
-#encode param image quality 0 to 100. default:95
-#if you want to shrink data size, choose low image quality.
-
-
-def startCamera():
-    img_counter = 0
-    encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),60]
+def send_data():
     while True:
-        ret, frame = cam.read()
-    
-        frame = imutils.resize(frame, width=320,height=240)
-    
-        frame = cv2.flip(frame,180)
-        result, image = cv2.imencode('.jpg', frame, encode_param)
-        data = pickle.dumps(image, 0)
-        size = len(data)
-
-        if img_counter%5==0:
-            if len(data)>0:
-                cameraSocket.sendall(struct.pack(">L", size) + data)
-            #cv2.imshow('client',frame)
-            
-        img_counter += 1
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        # Capture video frame
+        ret, frame = cap.read()
+        if not ret:
             break
-        
 
-    cam.release()
+        # Record audio frame
+        audio_frame = stream.read(1024)
 
+        # Combine video and audio frames into a JSON object
+        data = {'video': frame.tolist(), 'audio': np.frombuffer(audio_frame, dtype=np.int16).tolist()}
+        json_data = json.dumps(data)
 
-def startSound():
-    p = pyaudio.PyAudio()
-    stream_in = p.open(format=FORMAT,
-                        channels=CHANNELS,
-                        rate=RATE,
-                        input=True,
-                        frames_per_buffer=CHUNK_SIZE)
-    while True:
-        sounddata = stream_in.read(CHUNK_SIZE)
-        if len(sounddata)>0:
-            soundSocket.sendall(sounddata)
-        
-        
+        # Send the JSON object to the WebSocket server
+        ws.send(json_data)
 
+def on_open(ws):
+    # Start the thread that sends data to the WebSocket server
+    threading.Thread(target=send_data).start()
 
+if __name__ == '__main__':
+    # Connect to the WebSocket server
+    ws.connect('ws://192.168.16.103:8077')
 
+    # Set the on_open callback function
+    ws.on_open = on_open
 
-cameraClient= threading.Thread(target=startCamera)
-cameraClient.start()
-
-soundClient= threading.Thread(target=startSound)
-soundClient.start()
+    # Wait for the WebSocket connection to close
+    ws.run_forever()
