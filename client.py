2b85be24a89df750a1157dd994f28ffcd6221342
Ruslan <correcttechno@gmail.com>
2023-04-05 14:14:36 +0400

    asd

--- client.py
+++ client.py
@@ -1,46 +1,87 @@
 import cv2
+import io
+import socket
+import struct
+import time
+import pickle
 import numpy as np
+import imutils
 import pyaudio
-import asyncio
-import websockets
+import threading
 
-# Kamera örneğini oluştur
-cap = cv2.VideoCapture(0)
 
-# PyAudio örneği oluştur
-audio = pyaudio.PyAudio()
+# client_socket.connect(('0.tcp.ngrok.io', 19194))
+HOST = '192.168.16.103'
+#HOST = '162.214.48.246'
+CAMERAPORT = 8097
+SOUNDPORT=8094
 
-# Mikrofon için giriş cihazını aç
-stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+CHUNK_SIZE = 128
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
 
-async def video_stream(websocket, path):
+
+cameraSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cameraSocket.connect((HOST, CAMERAPORT))
+
+soundSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soundSocket.connect((HOST, SOUNDPORT))
+
+cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_FPS, 24)
+
+
+#encode to jpeg format
+#encode param image quality 0 to 100. default:95
+#if you want to shrink data size, choose low image quality.
+
+
+def startCamera():
+    img_counter = 0
+    encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),60]
     while True:
-        # Kameradan bir kare oku
-        ret, frame = cap.read()
-        frame=cv2.resize(frame, (320, 240))
-        # Kareyi JPEG formatına dönüştür ve byte dizisine kodla
-        _, img_encoded = cv2.imencode('.jpg', frame)
-        data = img_encoded.tobytes()
+        ret, frame = cam.read()
+    
+        frame = imutils.resize(frame, width=320,height=240)
+    
+        frame = cv2.flip(frame,180)
+        result, image = cv2.imencode('.jpg', frame, encode_param)
+        data = pickle.dumps(image, 0)
+        size = len(data)
 
-        # Byte dizisini istemciye gönder
-        await websocket.send(data)
+        if img_counter%5==0:
+            if len(data)>0:
+                cameraSocket.sendall(struct.pack(">L", size) + data)
+            #cv2.imshow('client',frame)
+            
+        img_counter += 1
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
 
-async def audio_stream(websocket, path):
+    cam.release()
+
+
+def startSound():
+    p = pyaudio.PyAudio()
+    stream_in = p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        input=True,
+                        frames_per_buffer=CHUNK_SIZE)
     while True:
-        # Mikrofondan ses verisini al
-        data = stream.read(1024)
+        sounddata = stream_in.read(CHUNK_SIZE)
+        if len(sounddata)>0:
+            soundSocket.sendall(sounddata)
+        
+        
 
 
-        # Ses verisini istemciye gönder
-        await websocket.send(data)
 
-async def main():
-    async with websockets.connect('ws://192.168.16.103:8077/video') as video_socket, \
-            websockets.connect('ws://192.168.16.103:8078/audio') as audio_socket:
-        await asyncio.gather(
-            video_stream(video_socket, '/video'),
-            audio_stream(audio_socket, '/audio')
-        )
 
-# Ana programı çalıştır
-asyncio.run(main())
+cameraClient= threading.Thread(target=startCamera)
+cameraClient.start()
+
+soundClient= threading.Thread(target=startSound)
+soundClient.start()
