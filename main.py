5a7a9c2e89e16c6812c67d5b05fc7aa85bf4d27f
Ruslan <ruslan.recebli@correcttechno.com>
2023-04-15 13:25:49 +0400

    asd

--- main.py
+++ main.py
@@ -1,65 +1,75 @@
-import tkinter as tk
 import cv2
+import tkinter as tk
 from PIL import Image, ImageTk
 
+class VideoCapture:
+    def __init__(self, video_source=0):
+        self.cap = cv2.VideoCapture(video_source)
+        if not self.cap.isOpened():
+            raise ValueError("Unable to open video source", video_source)
+        self.width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    def __del__(self):
+        if self.cap.isOpened():
+            self.cap.release()
+
+    def get_frame(self):
+        if self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if ret:
+                return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            else:
+                return None
+        else:
+            return None
+
 class App:
-    def __init__(self, window, window_title):
+    def __init__(self, window, window_title, video_source=0):
         self.window = window
         self.window.title(window_title)
-        
-        # pencereyi ikiye böl
-        self.canvas = tk.Canvas(window, width=600, height=480)
-        self.canvas.pack(side=tk.LEFT)
-        
-        # hesap makinesi widget'ını oluşturun
-        self.calculator_frame = tk.Frame(window, bg="white", bd=5)
-        self.calculator_frame.place(relx=0.7, rely=0.1, relwidth=0.25, relheight=0.8, anchor="n")
-        self.label = tk.Label(self.calculator_frame, font=("Arial", 10), bg="white", anchor="e")
-        self.label.place(relx=0, rely=0, relwidth=1, relheight=0.2)
-        self.equation = ""
-        
-        # hesap makinesi butonlarını oluşturun
-        buttons = ["C", "DEL", "/", "x",
-                   "7", "8", "9", "-",
-                   "4", "5", "6", "+",
-                   "1", "2", "3", "=",
-                   "0", ".", "(", ")"]
-        x = 0
-        y = 0
-        for button in buttons:
-            command = lambda x=button: self.button_click(x)
-            tk.Button(self.calculator_frame, text=button, bg="white", fg="black", command=command).place(relx=x, rely=y, relwidth=0.25, relheight=0.2)
-            x += 0.25
-            if x >= 1:
-                x = 0
-                y += 0.2
-        
-        # video akışı widget'ını oluşturun
-        self.video = cv2.VideoCapture(0)
-        self.delay = 15 # milisaniye cinsinden
+        self.video_source = video_source
+
+        # video source
+        self.vid = VideoCapture(self.video_source)
+
+        # canvas for displaying video
+        self.canvas = tk.Canvas(window, width=self.vid.width, height=self.vid.height)
+        self.canvas.pack()
+
+        # button to capture video
+        self.btn_snapshot = tk.Button(window, text="Snapshot", command=self.snapshot)
+        self.btn_snapshot.pack(anchor=tk.CENTER, expand=True)
+
+        # update the video display
+        self.delay = 15
         self.update()
+
         self.window.mainloop()
-    
-    def button_click(self, key):
-        if key == "C":
-            self.equation = ""
-        elif key == "DEL":
-            self.equation = self.equation[:-1]
-        elif key == "=":
-            try:
-                self.equation = str(eval(self.equation))
-            except:
-                self.equation = "Hata!"
-        else:
-            self.equation += str(key)
-        self.label.config(text=self.equation)
-        
+
+    def snapshot(self):
+        # get a frame from the video source
+        frame = self.vid.get_frame()
+
+        # convert to PIL image format
+        image = Image.fromarray(frame)
+
+        # save image
+        image.save("snapshot.png")
+
     def update(self):
-        ret, frame = self.video.read()
-        if ret:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+        # get a frame from the video source
+        frame = self.vid.get_frame()
+
+        if frame is not None:
+            # convert to PIL image format
+            image = Image.fromarray(frame)
+
+            # show image on canvas
+            self.photo = ImageTk.PhotoImage(image=image)
             self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+
         self.window.after(self.delay, self.update)
 
-App(tk.Tk(), "Hesap Makinesi ve Kamera Uygulaması")
+# create the app
+App(tk.Tk(), "Tkinter Video Player")
