e658d367373d1ea776609f7435b6f0aac14665c9
Ruslan <correcttechno@gmail.com>
2023-04-17 14:09:36 +0400

    sad

--- test.py
+++ test.py
@@ -1,194 +1,86 @@
-import threading
 import tkinter as tk
-import tkinter.font as tkFont
-import cv2
+
+class Calculator:
+    def __init__(self, master):
+        self.master = master
+        master.title("Hesap Makinesi")
+
+        # Metin kutusu oluşturma
+        self.display = tk.Entry(master, width=25, font=('Arial', 14))
+        self.display.grid(row=0, column=0, columnspan=4, pady=5)
+
+        # Rakam tuşlarını oluşturma
+        self.button_1 = tk.Button(master, text="1", width=5, height=2, font=('Arial', 14),
+                                  command=lambda: self.button_click('1'))
+        self.button_2 = tk.Button(master, text="2", width=5, height=2, font=('Arial', 14),
+                                  command=lambda: self.button_click('2'))
+        self.button_3 = tk.Button(master, text="3", width=5, height=2, font=('Arial', 14),
+                                  command=lambda: self.button_click('3'))
+        self.button_4 = tk.Button(master, text="4", width=5, height=2, font=('Arial', 14),
+                                  command=lambda: self.button_click('4'))
+        self.button_5 = tk.Button(master, text="5", width=5, height=2, font=('Arial', 14),
+                                  command=lambda: self.button_click('5'))
+        self.button_6 = tk.Button(master, text="6", width=5, height=2, font=('Arial', 14),
+                                  command=lambda: self.button_click('6'))
+        self.button_7 = tk.Button(master, text="7", width=5, height=2, font=('Arial', 14),
+                                  command=lambda: self.button_click('7'))
+        self.button_8 = tk.Button(master, text="8", width=5, height=2, font=('Arial', 14),
+                                  command=lambda: self.button_click('8'))
+        self.button_9 = tk.Button(master, text="9", width=5, height=2, font=('Arial', 14),
+                                  command=lambda: self.button_click('9'))
+        self.button_0 = tk.Button(master, text="0", width=5, height=2, font=('Arial', 14),
+                                  command=lambda: self.button_click('0'))
+
+        # Operatör tuşlarını oluşturma
+        self.button_add = tk.Button(master, text="+", width=5, height=2, font=('Arial', 14),
+                                    command=lambda: self.button_click('+'))
+        self.button_subtract = tk.Button(master, text="-", width=5, height=2, font=('Arial', 14),
+                                         command=lambda: self.button_click('-'))
+        self.button_multiply = tk.Button(master, text="*", width=5, height=2, font=('Arial', 14),
+                                         command=lambda: self.button_click('*'))
+        self.button_divide = tk.Button(master, text="/", width=5, height=2, font=('Arial', 14),
+                                       command=lambda: self.button_click('/'))
+        self.button_equal = tk.Button(master, text="=", width=5, height=2, font=('Arial', 14),
+                                      command=self.calculate)
+        self.button_clear = tk.Button(master, text="C", width=5, height=2, font=('Arial', 14),
+                                      command=self.clear)
+
+        # Tuşları düzenle
+                # Tuşları düzenleme
+        self.button_1.grid(row=3, column=0, padx=5, pady=5)
+        self.button_2.grid(row=3, column=1, padx=5, pady=5)
+        self.button_3.grid(row=3, column=2, padx=5, pady=5)
+        self.button_4.grid(row=2, column=0, padx=5, pady=5)
+        self.button_5.grid(row=2, column=1, padx=5, pady=5)
+        self.button_6.grid(row=2, column=2, padx=5, pady=5)
+        self.button_7.grid(row=1, column=0, padx=5, pady=5)
+        self.button_8.grid(row=1, column=1, padx=5, pady=5)
+        self.button_9.grid(row=1, column=2, padx=5, pady=5)
+        self.button_0.grid(row=4, column=0, padx=5, pady=5)
+        self.button_add.grid(row=1, column=3, padx=5, pady=5)
+        self.button_subtract.grid(row=2, column=3, padx=5, pady=5)
+        self.button_multiply.grid(row=3, column=3, padx=5, pady=5)
+        self.button_divide.grid(row=4, column=3, padx=5, pady=5)
+        self.button_equal.grid(row=4, column=2, padx=5, pady=5)
+        self.button_clear.grid(row=4, column=1, padx=5, pady=5)
+
+    def button_click(self, number):
+        current = self.display.get()
+        self.display.delete(0, tk.END)
+        self.display.insert(0, str(current) + str(number))
+
+    def clear(self):
+        self.display.delete(0, tk.END)
+
+    def calculate(self):
+        result = eval(self.display.get())
+        self.display.delete(0, tk.END)
+        self.display.insert(0, result)
 
 
 root = tk.Tk()
+root.attributes("-fullscreen", True)
 
 
-
-
-
-
-display=tk.Entry(root)
-display["borderwidth"] = "1px"
-ft = tkFont.Font(family='Roboto',size=30)
-display["font"] = ft
-display["fg"] = "#333333"
-display["justify"] = "center"
-display["text"] = "Entry"
-display.place(x=670,y=40,width=259,height=60)
-
-
-canvas = tk.Canvas(root)
-canvas.place(x=70,y=100,width=500,height=400)
-canvas.config(background="#000000",borderwidth=0)
-
-
-#canvas.pack()
-
-Button1=tk.Button(root)
-Button1["bg"] = "#efefef"
-ft = tkFont.Font(family='Roboto',size=28)
-Button1["font"] = ft
-Button1["fg"] = "#000000"
-Button1["justify"] = "center"
-Button1["text"] = "1"
-Button1["relief"] = "sunken"
-Button1.place(x=670,y=110,width=80,height=80)
-Button1["command"] = lambda:ButtonClick('1')
-
-Button2=tk.Button(root)
-Button2["bg"] = "#efefef"
-ft = tkFont.Font(family='Roboto',size=28)
-Button2["font"] = ft
-Button2["fg"] = "#000000"
-Button2["justify"] = "center"
-Button2["text"] = "2"
-Button2.place(x=760,y=110,width=80,height=80)
-Button2["command"] = lambda:ButtonClick(2)
-
-Button3=tk.Button(root)
-Button3["bg"] = "#efefef"
-ft = tkFont.Font(family='Roboto',size=28)
-Button3["font"] = ft
-Button3["fg"] = "#000000"
-Button3["justify"] = "center"
-Button3["text"] = "3"
-Button3.place(x=850,y=110,width=80,height=80)
-Button3["command"] = lambda:ButtonClick(3)
-
-Button4=tk.Button(root)
-Button4["bg"] = "#efefef"
-ft = tkFont.Font(family='Roboto',size=28)
-Button4["font"] = ft
-Button4["fg"] = "#000000"
-Button4["justify"] = "center"
-Button4["text"] = "4"
-Button4.place(x=670,y=200,width=80,height=80)
-Button4["command"] = lambda:ButtonClick(4)
-
-Button5=tk.Button(root)
-Button5["bg"] = "#efefef"
-ft = tkFont.Font(family='Roboto',size=28)
-Button5["font"] = ft
-Button5["fg"] = "#000000"
-Button5["justify"] = "center"
-Button5["text"] = "5"
-Button5.place(x=760,y=200,width=80,height=80)
-Button5["command"] = lambda:ButtonClick(5)
-
-Button6=tk.Button(root)
-Button6["bg"] = "#efefef"
-ft = tkFont.Font(family='Roboto',size=28)
-Button6["font"] = ft
-Button6["fg"] = "#000000"
-Button6["justify"] = "center"
-Button6["text"] = "6"
-Button6.place(x=850,y=200,width=80,height=80)
-Button6["command"] = lambda:ButtonClick(6)
-
-Button7=tk.Button(root)
-Button7["bg"] = "#efefef"
-ft = tkFont.Font(family='Roboto',size=28)
-Button7["font"] = ft
-Button7["fg"] = "#000000"
-Button7["justify"] = "center"
-Button7["text"] = "7"
-Button7.place(x=670,y=290,width=80,height=80)
-Button7["command"] =lambda:ButtonClick(7)
-
-Button8=tk.Button(root)
-Button8["bg"] = "#efefef"
-ft = tkFont.Font(family='Roboto',size=28)
-Button8["font"] = ft
-Button8["fg"] = "#000000"
-Button8["justify"] = "center"
-Button8["text"] = "8"
-Button8.place(x=760,y=290,width=80,height=80)
-Button8["command"] = lambda:ButtonClick(8)
-
-Button9=tk.Button(root)
-Button9["bg"] = "#efefef"
-ft = tkFont.Font(family='Roboto',size=28)
-Button9["font"] = ft
-Button9["fg"] = "#000000"
-Button9["justify"] = "center"
-Button9["text"] = "9"
-Button9.place(x=850,y=290,width=80,height=80)
-Button9["command"] = lambda:ButtonClick(9)
-
-GButton_184=tk.Button(root)
-GButton_184["bg"] = "#efefef"
-ft = tkFont.Font(family='Roboto',size=28)
-GButton_184["font"] = ft
-GButton_184["fg"] = "#000000"
-GButton_184["justify"] = "center"
-GButton_184["text"] = "*"
-GButton_184.place(x=670,y=380,width=80,height=80)
-
-GButton_735=tk.Button(root)
-GButton_735["bg"] = "#efefef"
-ft = tkFont.Font(family='Roboto',size=28)
-GButton_735["font"] = ft
-GButton_735["fg"] = "#000000"
-GButton_735["justify"] = "center"
-GButton_735["text"] = "0"
-GButton_735.place(x=760,y=380,width=80,height=80)
-GButton_735["command"] = lambda:ButtonClick(0)
-
-GButton_690=tk.Button(root)
-GButton_690["bg"] = "#efefef"
-ft = tkFont.Font(family='Roboto',size=28)
-GButton_690["font"] = ft
-GButton_690["fg"] = "#000000"
-GButton_690["justify"] = "center"
-GButton_690["text"] = "#"
-GButton_690.place(x=850,y=380,width=80,height=80)
- 
-
-GButton_339=tk.Button(root)
-GButton_339["bg"] = "#5fb878"
-ft = tkFont.Font(family='Roboto',size=18)
-GButton_339["font"] = ft
-GButton_339["fg"] = "#000000"
-GButton_339["justify"] = "center"
-GButton_339["text"] = "CALL"
-GButton_339.place(x=670,y=470,width=260,height=49)
-
-   
-
-def ButtonClick(number):
-    current = display.get()
-    display.delete(0, tk.END)
-    display.insert(0, str(current) + str(number))
-
-   
-
-
-
-#root.attributes("-fullscreen", True)
-
-# Navigasyon çubuğunu gizle
-root.overrideredirect(True)
-root.configure(background="#000000")
-root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
-
-
-
-#root.after(10, show_frame)
-
-""" 
-#setting title
-root.title("MSB Distant Door Bell")
-#setting window size
-width=1024
-height=600
-screenwidth = root.winfo_screenwidth()
-screenheight = root.winfo_screenheight()
-alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-root.geometry(alignstr)
-root.resizable(width=False, height=False)
- """
+calculator = Calculator(root)
 root.mainloop()
