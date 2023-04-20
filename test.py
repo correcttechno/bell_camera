8d258ff6aa8c0b1e8c1b2538ecf5fd0d151b28fc
Ruslan <ruslan.recebli@correcttechno.com>
2023-04-20 13:30:37 +0400

    asd

--- test.py
+++ test.py
@@ -1,30 +1,19 @@
 import pyaudio
-
-CHUNK = 1024
+import cv2
+CHUNK_SIZE = 128
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-RECORD_SECONDS = 5
+
 
 p = pyaudio.PyAudio()
+stream_in = p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        input=True,
+                        frames_per_buffer=CHUNK_SIZE,
+                        input_device_index=2
+                        )
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                input_device_index=None,
-                frames_per_buffer=CHUNK)
 
-frames = []
-
-print("Recording...")
-
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
-
-print("Finished recording.")
-
-stream.stop_stream()
-stream.close()
-p.terminate()
+sounddata = stream_in.read(CHUNK_SIZE)
