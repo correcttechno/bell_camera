9ca53dab6f16e60a418f62d6aa253857b345387d
Ruslan <ruslan.recebli@correcttechno.com>
2023-04-20 13:06:36 +0400

    sd

--- test.py
+++ test.py
@@ -1,27 +1,28 @@
 import pyaudio
-import pulsectl
+import cv2
+CHUNK_SIZE = 128
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
 
 p = pyaudio.PyAudio()
-
-# PulseAudio client instance oluşturun
-pulse = pulsectl.Pulse('my-client-name')
-
-# Kayıt için PulseAudio stream oluşturun
-stream = pulse.record_stream('cart 2')
-
-# Kayıt için PyAudio stream oluşturun
-stream_py = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, input_device_index=None, frames_per_buffer=1024, stream_callback=None)
-
-# Kayıt işlemini başlatın
-stream_py.start_stream()
-stream.start()
-
-# Kaydedilen ses verilerini işleyin
+stream_in = p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        input=True,
+                        frames_per_buffer=CHUNK_SIZE,
+                        input_device_index=2
+                        )
+stream_out = p.open(format=FORMAT,
+                               channels=CHANNELS,
+                               rate=RATE,
+                               output=True,
+                               frames_per_buffer=CHUNK_SIZE,
+                               output_device_index=2
+                               )
 while True:
-    data = stream.read(1024)
-    stream_py.write(data)
-
-# Kayıt işlemini sonlandırın
-stream_py.stop_stream()
-stream_py.close()
-p.terminate()
+        sounddata = stream_in.read(CHUNK_SIZE)
+        stream_out.write(sounddata)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
