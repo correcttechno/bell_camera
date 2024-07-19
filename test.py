c069009de99492985bd806ce176341b6eecf94d4
My Name <myemail@example.com>
2024-07-19 08:20:22 +0400

    asdas

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import pyaudio
 
-p = pyaudio.PyAudio()
-info = p.get_host_api_info_by_index(0)
-numdevices = info.get('deviceCount')
+def list_devices():
+    p = pyaudio.PyAudio()
+    info = p.get_host_api_info_by_index(0)
+    num_devices = info.get('deviceCount')
+    for i in range(0, num_devices):
+        device_info = p.get_device_info_by_host_api_device_index(0, i)
+        print(f"Device ID {i} - {device_info['name']}")
 
-for i in range(0, numdevices):
-    device_info = p.get_device_info_by_host_api_device_index(0, i)
-    if device_info.get('maxInputChannels') > 0:
-        print(f"Device ID {i} - {device_info.get('name')}")
+list_devices()
