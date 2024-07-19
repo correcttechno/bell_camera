f9b74b61e60e80b014ac55cf50bfebd648714ed7
My Name <myemail@example.com>
2024-07-19 08:30:43 +0400

    aas

--- test.py
+++ test.py
@@ -1,11 +1,25 @@
 import pyaudio
 
-def list_devices():
-    p = pyaudio.PyAudio()
-    info = p.get_host_api_info_by_index(0)
-    num_devices = info.get('deviceCount')
-    for i in range(0, num_devices):
-        device_info = p.get_device_info_by_host_api_device_index(0, i)
-        print(f"Device ID {i} - {device_info['name']}")
+# PyAudio örneği oluşturun
+audio = pyaudio.PyAudio()
 
-list_devices()
+# USB ses aygıtını bulun
+info = audio.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+usb_device_index = None
+
+print("Sistemdeki ses aygıtları:")
+
+for i in range(0, numdevices):
+    device_info = audio.get_device_info_by_host_api_device_index(0, i)
+    device_name = device_info.get('name')
+    print(f"İndeks: {i}, Aygıt Adı: {device_name}")
+    if "USB" in device_name:
+        usb_device_index = i
+
+if usb_device_index is None:
+    print("USB ses aygıtı bulunamadı.")
+else:
+    print(f"USB ses aygıtı indeksi: {usb_device_index}")
+
+audio.terminate()
