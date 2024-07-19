a64226940cc2b5e9fbd260f6a08f5d03e7694fe2
My Name <myemail@example.com>
2024-07-19 08:14:50 +0400

    ch

--- client.py
+++ client.py
@@ -83,6 +83,7 @@ audio = pyaudio.PyAudio()
 
 stream_in = audio.open(format=FORMAT, channels=CHANNELS,
                 rate=RATE, input=True,
+                input_device_index=0,
                 frames_per_buffer=CHUNK_SIZE)
     
 
