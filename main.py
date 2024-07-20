d07883a0a86949d71aeb2e401f5a58952760f23d
My Name <myemail@example.com>
2024-07-20 18:43:32 +0400

    as

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ from faceid import setFaceIDCameraFrame
 
 cam = cv2.VideoCapture(0)
 
+cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
 app = Flask(__name__)
 
 def generate_frames():
