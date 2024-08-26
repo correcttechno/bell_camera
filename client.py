5b97b4c4fa3979f587ab5bba1539f270a53ddac2
My Name <myemail@example.com>
2024-08-26 16:23:30 +0400

    sound send and recive , video server fixed

--- client.py
+++ client.py
@@ -1,44 +1,65 @@
+import pickle
 import socket
+import struct
+import time
+import cv2
 import pyaudio
 import threading
 
-# Sunucu ayarları
-HOST = '127.0.0.1'  # Sunucunun IP adresi
-PORT = 8094
 
-# Ses ayarları
+HOST = '127.0.0.1'  
+AUDIOPORT = 8094
+VIDEOPORT = 8095
+
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 CHUNK_SIZE = 4096
 
-# İstemci soketi oluşturuluyor
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
+audioClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+audioClientSocket.connect((HOST, AUDIOPORT))
+
+videoClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+videoClientSocket.connect((HOST, VIDEOPORT))
 
 audio = pyaudio.PyAudio()
+video = cv2.VideoCapture(0)
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 15]
 
-# Giriş (mikrofon) stream
 stream_in = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True,
                        frames_per_buffer=CHUNK_SIZE)
 
-# Çıkış (hoparlör) stream
 stream_out = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True,
                         frames_per_buffer=CHUNK_SIZE)
-
-def send_audio():
-    """Mikrofon verisini sunucuya gönderir"""
+#send audio data
+def sendAudio():
     while True:
         data = stream_in.read(CHUNK_SIZE)
-        client_socket.sendall(data)
-
-def receive_audio():
-    """Sunucudan gelen veriyi hoparlörden çalar"""
+        audioClientSocket.sendall(data)
+#recive audio data
+def receiveAudio():
     while True:
-        data = client_socket.recv(CHUNK_SIZE)
+        data = audioClientSocket.recv(CHUNK_SIZE)
         if data:
             stream_out.write(data)
+#send video data
+def sendVideo():
+    while True:
+        success, cleanFrame = video.read()
+    
+        if cleanFrame is not None:
+            frame = cleanFrame
+            # frame = imutils.resize(frame, width=320,height=240)
+            frame = cv2.flip(frame, 180)
+            result, image = cv2.imencode('.jpg', frame, encode_param)
+            data = pickle.dumps(image, 0)
+            size = len(data)
+            if len(data) > 0:
+                videoClientSocket.sendall(struct.pack(">L", size) + data)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# İki thread başlatılıyor: biri ses göndermek diğeri almak için
-threading.Thread(target=send_audio).start()
-threading.Thread(target=receive_audio).start()
+#start functions
+threading.Thread(target=sendAudio).start()
+threading.Thread(target=receiveAudio).start()
+threading.Thread(target=sendVideo).start()
--- server.py
+++ server.py
@@ -1,5 +1,10 @@
+import pickle
 import socket
+import socketserver
+import struct
 import threading
+
+import cv2
 
 #import pyaudio
 
@@ -8,57 +13,110 @@ import threading
 #stream_out = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, output=True)
 #stream_in = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True,frames_per_buffer=4096)
 
+HOST = '0.0.0.0' 
+AUDIOPORT = 8094
+VIDEOPORT = 8095
 
+audioClients = []
+videoClients = []
 
-# Sunucu ayarları
-HOST = '0.0.0.0'  # Sunucu tüm IP adreslerinden gelen bağlantıları kabul eder.
-PORT = 8094
+audioServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+audioServer.bind((HOST, AUDIOPORT))
+audioServer.listen(5)
 
-# İstemcileri takip etmek için bir liste
-clients = []
+videoServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+videoServer.bind((HOST, VIDEOPORT))
+videoServer.listen(5)
 
-# Sunucu soketi oluşturuluyor
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen(5)
-print(f"Server started on {HOST}:{PORT}")
-
-def broadcast(data, conn):
+#Audio Server
+def audioBroadcast(data, conn):
     #stream_out.write(data)
-
-    """Bir istemciden gelen veriyi diğer tüm istemcilere gönderir"""
-    for client in clients:
+    for client in audioClients:
         if client != conn:
             try:
                 client.sendall(data)
             except:
-               clients.remove(client)
-
-def handle_client(conn, addr):
-    """Her istemci bağlantısı için bu fonksiyon çalışır"""
+               audioClients.remove(client)
+#audio handle
+def audioHandleClient(conn, addr):
     print(f"New connection from {addr}")
-    clients.append(conn)
+    audioClients.append(conn)
     
     while True:
         try:
-            data = conn.recv(4096)  # Veriyi al
+            data = conn.recv(4096)  
             if not data:
                 break
-            broadcast(data, conn)  
+            audioBroadcast(data, conn)  
         except:
             break
     
-    # İstemci bağlantısı kesildiğinde
     print(f"Connection from {addr} closed")
-    clients.remove(conn)
+    audioClients.remove(conn)
     conn.close()
-
-def start_server():
-    """Sunucuyu başlatır ve istemcilerden gelen bağlantıları kabul eder"""
+#start audio server
+def startAudioServer():
     while True:
-        conn, addr = server.accept() 
-        threading.Thread(target=handle_client, args=(conn, addr)).start()
+        conn, addr = audioServer.accept() 
+        threading.Thread(target=audioHandleClient, args=(conn, addr)).start()
         
 
-# Sunucuyu başlat
-start_server()
+#video Server
+def videoBroadcast(data, conn):
+    #stream_out.write(data)
+    for client in videoClients:
+        if client != conn:
+            try:
+                client.sendall(data)
+            except:
+               videoClients.remove(client)
+#video handle
+def videoHandleClient(conn, addr):
+
+    print(f"New connection from {addr}")
+    videoClients.append(conn)
+
+    data = b""
+    payload_size = struct.calcsize(">L")
+    encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
+    while True:
+        try:
+            while len(data) < payload_size:
+                data += conn.recv(50*1024)
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack(">L", packed_msg_size)[0]
+            while len(data) < msg_size:
+                data += conn.recv(50*1024)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+            frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+            frame = cv2.flip(frame,180)
+            result, image = cv2.imencode('.jpg', frame, encode_param)
+            if conn is not None:
+                mydata = struct.pack('>L', len(image.tobytes())) + image.tobytes()
+                videoBroadcast(mydata, conn)
+        except:
+            break
+            
+    print(f"Connection from {addr} closed")
+    videoClients.remove(conn)
+    conn.close()
+#start video server
+def startVideoServer():
+    while True:
+        conn, addr = videoServer.accept() 
+        threading.Thread(target=videoHandleClient, args=(conn, addr)).start()
+        
+
+#start servers
+threading.Thread(target=startAudioServer).start()
+threading.Thread(target=startVideoServer).start()
+
+print("AUDIO SERVER STARTED")
+print("VIDEO SERVER STARTED")
+
+
+
+
