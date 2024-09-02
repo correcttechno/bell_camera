a7543f8c2ba7203a5aee1586f9204520b2e10f26
My Name <myemail@example.com>
2024-09-02 09:53:18 +0400

    enabled face id

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import cv2
 from flask import Flask, Response, render_template, render_template_string
 from client import setClientCameraFrame
+from faceid import setFaceIDCameraFrame
 #from faceid import setFaceIDCameraFrame
 
 
@@ -13,7 +14,7 @@ cap = cv2.VideoCapture(0)
 
 #start face id and network
 setClientCameraFrame(cap)
-#setFaceIDCameraFrame(cap)
+setFaceIDCameraFrame(cap)
 
 #generate http live
 def generate_frames():
